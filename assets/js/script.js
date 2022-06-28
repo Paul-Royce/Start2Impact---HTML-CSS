a18ee696b568402a4c1de8ca8330ccb846a8448d
Paul <paulroycefinance@gmail.com>
2022-06-28 19:47:14 +0200

    deleting script

--- assets/js/script.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const userNameOne = document.querySelector('.user-name');
-const userNameTwo = document.querySelector('.username');
-    
-let askUserName= prompt("Hello here is Paul. What's your name?", 'name');
-userNameOne.textContent = askUserName;
-userNameTwo.textContent = askUserName;
